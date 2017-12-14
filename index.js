715d2b3c8941d7dd8452e354cb69951da123a64d
lexman02 <lexman41@gmail.com>
2017-12-13 19:02:58 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const botSettings = require("./botsettings.json");
+const Discord = require('discord.js');
+const prefix = botSettings.prefix
+const ce = require("embed-creator");
+
+const bot = new Discord.Client();
+
+bot.on("ready", async () => {
+    console.log(`${bot.user.username} is on and monitoring!`);
+
+    try {
+        let link = await bot.generateInvite(botSettings.permissions);
+        console.log(link);
+    } catch(e) {
+        console.log(e.stack);
+    }
+});
+
+bot.on('message', async message => {
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return;
+
+    let messageArray = message.content.split(" ");
+    let command = messageArray[0];
+    let args = messageArray.slice(1);
+
+    if(!command.startsWith(prefix)) return;
+
+    if(command === `${prefix}userinfo`) {
+        let embed = msg.channel.send(ce(
+            "9B59B6", 
+            {"name": "msg.author.username"},
+            "null",
+            "This is the user's info!",
+            [{"name": "Full Username", "value": `${message.author.username}#${message.author.discriminator}`},
+             {"name": "ID", "value": `${message.author.id}`}]
+        ));
+        /*let embed = new Discord.RichEmbed();
+            .setAuthor(`${message.author.username}`);
+            .setDescription("This is the user's info!");
+            .setColor("#9B59B6");
+            .addField("Full Username", `${message.author.username}#${message.author.discriminator}`);
+            .addField("ID", `${message.author.id}`);
+            .addField("Created At", `${message.author.createdAt}`);   
+
+        message.channel.sendEmbed(embed);*/
+
+        return;
+    }
+});
+
+bot.login(botSettings.token);
